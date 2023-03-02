43a1d65b50ba47886f0110adb58a0b1fa574677f
wonwon <hakpower303@naver.com>
2023-03-02 19:18:38 +0900

    add sql file

---
+++ src/main/resources/insertData.sql
@@ -0,0 +1,63 @@
+/*
+ * 우선 member 데이터부터 insert 하셔야 합니다.
+ */
+
+/*
+ * member data
+ */
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user1',md5('1234qwer'),'user1','user1@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user2',md5('1234qwer'),'user2','user2@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user3',md5('1234qwer'),'user3','user3@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user4',md5('1234qwer'),'user4','user4@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user5',md5('1234qwer'),'user5','user5@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user6',md5('1234qwer'),'user6','user6@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user7',md5('1234qwer'),'user7','user7@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user8',md5('1234qwer'),'user8','user8@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user9',md5('1234qwer'),'user9','user9@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user10',md5('1234qwer'),'user10','user10@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user11',md5('1234qwer'),'user11','user11@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user12',md5('1234qwer'),'user12','user12@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user13',md5('1234qwer'),'user13','user13@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user14',md5('1234qwer'),'user14','user14@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user15',md5('1234qwer'),'user15','user15@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user16',md5('1234qwer'),'user16','user16@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user17',md5('1234qwer'),'user17','user17@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user18',md5('1234qwer'),'user18','user18@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user19',md5('1234qwer'),'user19','user19@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user20',md5('1234qwer'),'user20','user20@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user21',md5('1234qwer'),'user21','user21@test.com',30,'M',now(),10);
+insert into member(m_id,password,name,email,age,gender,reg_date,level) values('user22',md5('1234qwer'),'user22','user22@test.com',30,'M',now(),10);
+
+/*
+ * restaurant data
+ */
+
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user1','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user13','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user19','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user2','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user1','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user3','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user1','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user2','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user2','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user2','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user1','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user1','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user3','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user1','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user5','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user1','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user4','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user14','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user5','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user5','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user18','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user6','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user1','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user7','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user19','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user10','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user10','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user9','test','test','test',null,null,null,null,null,0,0,now());
+insert into restaurant(m_id,name,addr,content,img1,img2,img3,img4,img5,loc_x,loc_y,reg_date) values('user8','test','test','test',null,null,null,null,null,0,0,now());
